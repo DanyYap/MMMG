3a3cca0825754d1dc28af0c14d4c76dea8ca9c99
DanyYap <124513296+DanyYap@users.noreply.github.com>
2024-10-13 22:06:25 +0800

    Testing_Tag

--- Assets/Scripts/Game Mechanic/FireExtinguishing.cs
+++ Assets/Scripts/Game Mechanic/FireExtinguishing.cs
@@ -2,48 +2,8 @@ using UnityEngine;
 
 public class FireExtinguishing : MonoBehaviour
 {
-    public ParticleSystem waterParticleSystem; 
-    public ParticleSystem fireParticleSystem; 
-    public Collider fireCollider; 
-
-    void Start()
+    void OnCollisionEnter(Collision collision)
     {
-        
-        var waterCollision = waterParticleSystem.collision;
-        waterCollision.enabled = true;
+        Debug.Log("Collide with object");
     }
-
-    void Update()
-    {
-        if (IsWaterExtinguishingFire())
-        {
-            ExtinguishFire();
-        }
-    }
-
-    bool IsWaterExtinguishingFire()
-    {
-        ParticleSystem.Particle[] particles = new ParticleSystem.Particle[waterParticleSystem.main.maxParticles];
-        int numParticlesAlive = waterParticleSystem.GetParticles(particles);
-
-        for (int i = 0; i < numParticlesAlive; i++)
-        {
-            if (fireCollider.bounds.Contains(particles[i].position))
-            {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    void ExtinguishFire()
-    {
-        if (fireParticleSystem.isPlaying)
-        {
-            fireParticleSystem.Stop();
-            Debug.Log("Fire is extinguished!");
-        }
-    }
-    
 }
