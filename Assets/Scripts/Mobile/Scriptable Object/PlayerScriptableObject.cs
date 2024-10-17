e0e40b77fa1babedfe5dd406f427e6a21467497f
CallMeWho <lewxiuzhen@gmail.com>
2024-10-17 14:30:14 +0800

    Have removed unused stuff
    - photon
    - netcode

--- Assets/Scripts/Mobile/Scriptable Object/PlayerScriptableObject.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "PlayerScriptableObject", menuName = "Scriptable Objects/PlayerScriptableObject")]
-public class PlayerScriptableObject : ScriptableObject
-{
-    public GameObject Player1;
-    public GameObject Player2;
-}
